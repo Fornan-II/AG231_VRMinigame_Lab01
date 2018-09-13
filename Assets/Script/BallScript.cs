56f87ec55016d06eb19547ce4efbc42023aba93a
danalemay <danalemay@users.noreply.github.com>
2018-09-13 11:13:17 -0400

    ball and paddle script

---
+++ Assets/Script/BallScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public GameObject spawnpt;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            gameObject.transform.position = spawnpt.transform.position;
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Script/PaddleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    public int speed = 5;
+    public int maxV = 10;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            GameObject ball = collision.gameObject;
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            if (rb.velocity.magnitude >= maxV)
+            {
+
+            }
+            else
+            {
+                rb.velocity *= speed;
+            }
+        }
+    }
+}
