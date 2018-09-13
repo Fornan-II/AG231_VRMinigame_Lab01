e7ba62c859e32a4bf73175e44519a7a5900c25c0
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-09-13 11:12:50 -0400

    Level

---
+++ Assets/BrickScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour {
+
+    public static int ballsRemaining = 0;
+    protected static int totalBallCount = 0;
+    public GameObject destroyParticlePrefab;
+    public float particleLifeTime;
+
+    protected virtual void Start()
+    {
+        totalBallCount++;
+        ballsRemaining++;
+    }
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            if(destroyParticlePrefab)
+            {
+                GameObject particle = Instantiate(destroyParticlePrefab, collision.transform.position, collision.transform.rotation);
+                Destroy(particle, particleLifeTime);
+            }
+            Destroy(gameObject);
+            ballsRemaining--;
+        }
+    }
+}
