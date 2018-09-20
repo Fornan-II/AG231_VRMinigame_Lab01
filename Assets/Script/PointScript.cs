bf59af75a2c4b2babee4ca3543c2cbdb194a66cc
danalemay <37815248+danalemay@users.noreply.github.com>
2018-09-20 09:39:18 -0400

    line pointer

---
+++ Assets/Script/PointScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointScript : MonoBehaviour {
+    LineRenderer laserLine;
+
+    void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        laserLine.enabled = false;
+    }	
+	
+	void Update () {
+        if (Input.GetButton("Oculus_GearVR_RIndexTrigger"))
+        {
+            RaycastHit hit;
+            laserLine.enabled = true;
+
+            Vector3 rayOrigin = gameObject.transform.position;
+            laserLine.SetPosition(0, gameObject.transform.position);
+
+            if (Physics.Raycast(rayOrigin, gameObject.transform.forward, out hit, 10))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (gameObject.transform.forward * 10));
+            }
+        }
+        else
+        {
+            laserLine.enabled = false;
+        }
+    }
+}
