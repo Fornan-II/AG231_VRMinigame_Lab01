a4cb5d158288e07267aed8142ddff704152b7887
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-10-02 11:41:42 -0400

    Changes

--- Assets/Script/MoveScript.cs
+++ Assets/Script/MoveScript.cs
@@ -16,7 +16,7 @@ public class MoveScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Vector3 newVelocity = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        Vector3 newVelocity = head.forward * Input.GetAxis("Horizontal") + head.right * Input.GetAxis("Vertical");
+        Vector3 newVelocity = head.forward * Input.GetAxis("Vertical") + head.right * Input.GetAxis("Horizontal");
         newVelocity.y = 0.0f;
         newVelocity *= moveSpeed * Time.deltaTime;
         rb.velocity = newVelocity;
